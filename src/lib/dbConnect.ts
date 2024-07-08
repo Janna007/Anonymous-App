9b6921bca24b7afab78650d79e9fe6bbc136e242
janna <jannakondeth5@gmail.com>
2024-07-08 10:51:41 +0530

    connect db

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+type connetionObject={
+    isConnected?:number
+}
+
+const connection :connetionObject={}
+
+async function dbConnect(){
+
+         if(connection.isConnected){
+            console.log("DB already connected") 
+            return
+         }
+          try {
+            const dbConnection= await mongoose.connect(process.env.MONGO_URL || "")
+            connection.isConnected=dbConnection.connections[0].readyState
+            console.log("  successfully Connected to db ")
+          } catch (error:any) {
+            console.log("DB connection failed",error.message)
+            process.exit(1)
+          }
+}
+
+export default dbConnect;
