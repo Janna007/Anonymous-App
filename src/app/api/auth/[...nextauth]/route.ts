0946fd2c65c0bef290f6ba797a08628722ce03e4
janna <jannakondeth5@gmail.com>
2024-07-10 11:34:12 +0530

    add authoptions and types

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,59 +1,7 @@
-import { NextAuthOptions } from 'next-auth';
-import dbConnect from "@/lib/dbConnect"
-import UserModel from "@/model/User"
-import CredentialsProvider from "next-auth/providers/credentials"
-import bcrypt from 'bcryptjs'
+import nextAuth from "next-auth";
+import { authOptions } from "./options";
 
 
-export const authOptions:NextAuthOptions={
-    providers: [
-        CredentialsProvider({
-         
-          id:'Credentials',
-          name: 'Credentials',
-      
-          credentials: {
-            username: { label: "email", type: "text" },
-            password: { label: "Password", type: "password" }
-          },
-          async authorize(credentials:any):Promise<any> {
-               await dbConnect()
-               
-             try {
-               const user= await UserModel.findOne({
-                   $or:[
-                       {email:credentials.identifier},
-                       {password:credentials.identifier}
-                   ]
-                })
-       
-                if(!user){
-                   throw new Error("User not found !! Invalid credentials")
-                }
-      
-                if(!user.isVerified){
-                  throw new Error("Verify your Account before Login!")
-                }
-                
-                const isPasswordCorrect=await bcrypt.compare(credentials.password,user.password)
-      
-                if(isPasswordCorrect){
-                   return user
-                }else{
-                  throw new Error("Check your password again!!")
-                  
-                }
-      
-      
-             } catch (error:any) {
-                throw new Error(error)
-             }
-             
-           
-          
-          }
-        })
-      ]
-      
-}
+const handler=nextAuth(authOptions)
 
+export {handler as GET ,handler as POST}
