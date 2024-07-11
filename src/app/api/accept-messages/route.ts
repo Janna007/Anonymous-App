2ad8e21b99f914f2e6301f5cd4ca147048aa9b25
janna <jannakondeth5@gmail.com>
2024-07-11 16:20:35 +0530

    add route for changing message accepting flag

---
+++ src/app/api/accept-messages/route.ts
@@ -0,0 +1,51 @@
+import dbConnect from "@/lib/dbConnect";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+import UserModel from "@/model/User";
+
+
+
+export async function POST(request:Request){
+     await dbConnect()
+
+     const session =await getServerSession(authOptions)
+     const user=session?.user
+
+     if(!session || !user){
+        return Response.json({
+            success:false,
+            message:"Not authenticated!!Please Signup"
+        },{status:404})
+     }
+     
+     const userId=user._id
+     const {acceptMessageFlag}=await request.json()
+
+    try {
+         const updatedUser=await UserModel.findByIdAndUpdate(userId,{
+            isAcceptingMessage:acceptMessageFlag
+         },{new:true})
+    
+         if(!updatedUser){
+            return Response.json({
+                success:false,
+                message:"error while updating the user message accepting state"
+            },{status:500})
+         }
+
+         return Response.json({
+            success:true,
+            message:"succesfully change the user message accepting state",
+            updatedUser
+        },{status:200})
+         
+    
+    } catch (error) {
+        console.log("Error while updating user state")
+        return Response.json({
+            success:false,
+            message:"Error while updating user state"
+        },{status:500})
+    }
+
+}
